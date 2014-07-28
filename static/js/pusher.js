a712e88afb1d4fbe532bfe370e3eae3bfa3aa3aa
Minseok Kim <minseok.kim89@gmail.com>
2014-07-28 17:57:47 +0900

    엔터로 메세지 보내도록

--- static/js/pusher.js
+++ static/js/pusher.js
@@ -4,7 +4,17 @@ $(document).ready(function(){
 	channel.bind('notification',function(data){
 		alert(data.username + ' is online.');
 	});
-	
+
+	channel.bind('chat',function(data){
+		$('#chat').append(data.username+": "+data.chat+"<br>");
+	});
+
+	$('#chat_cont').on('keydown', function(e){
+    	if(e.which==13) {
+    		e.preventDefault();
+			$('#button-send-chat').click();
+		}
+  	});
 	
 	$('#button-send-chat').click(function(){
 		var chat=$('#chat_cont').val();
@@ -30,32 +40,6 @@ $(document).ready(function(){
 		$('#chat_cont').val("");
 	});
 
-	$("#button-send-chat").on("keydown",function(){
-    	var chat=$('#chat_cont').val();
-		$.ajax({
-			url: '/chat',
-			type:'POST',
-			data:{"chat":chat},
-			// 여기까지는 데이터를 리로딩 하지 않고 보내는 것
-			success:function(response){
-				var result = $.parseJSON(response);
-				// if(result['content']=="bad"){
-				// 	$('#chat_err').text("Oops, you have to type anything!");
-				// }
-				console.log(result['content']);
-			},
-			error: function(){
-				console.log('error');
-			},
-			complete:function(){
-				console.log('complete');
-			}
-		});
-		$('#chat_cont').val("");
-  	});
 
 
-	channel.bind('chat',function(data){
-		$('#chat').append(data.username+": "+data.chat+"<br>");
-	});
 });
